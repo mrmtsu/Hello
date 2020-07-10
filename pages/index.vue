d9818b446b2c4414e5babf470b9c3f3c890f47d9
mrmtsu <81202muramatsu@gmail.com>
2020-07-11 02:20:23 +0900

    fist-commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <p>{{ message }}</p>
+    <hr />
+    <router-link to="/price">Price Page</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello Nuxt.js!"
+    };
+  }
+};
+</script>
+
+<style>
+</style>
